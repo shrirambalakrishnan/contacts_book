0775604b46fec0f1a64c9aaf9710fae19b2ccc12
shri <shriram@gmail.com>
2014-06-20 14:07:57 +0530

    Deletes previous photo while updating the contact(edit)

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,15 +1,14 @@
 class ContactsController < ApplicationController
+	$flag_edit=false # flag to find whether the request for photo upload is for new or edit
 	def new
 		@contact=Contact.new
+		$flag_edit=false
 	end
 
 	def create
 		@contact=Contact.create(contact_params)
-		name=params[:contact][:photo].original_filename
-		directory="public/data"
-		path=File.join(directory,name)
-		File.open(path, "wb") { |f| f.write(params[:contact][:photo].read) }
-		@contact.image_location=name
+		@name=savePhoto
+		@contact.image_location=@name
 		if @contact.save
 			redirect_to @contact
 		else
@@ -33,14 +32,37 @@ class ContactsController < ApplicationController
 
 	def edit
 		@contact=Contact.find(params[:id])
+		$flag_edit=true
 	end
 
 	def update
+		name=savePhoto
 		@contact=Contact.find(params[:id])
+		@contact.image_location=name
+		$flag_edit=false
 		if @contact.update(contact_params)
 			redirect_to contacts_path
 		else
 			render 'edit'
+		end
+	end
+	
+	def savePhoto
+		if !$flag_edit
+			name=params[:contact][:photo].original_filename
+			directory="public/data"
+			path=File.join(directory,name)
+			File.open(path, "wb") { |f| f.write(params[:contact][:photo].read) }
+			return name
+		else
+			@contact=Contact.find(params[:id])
+			name=@contact.image_location
+			File.delete("#{Rails.root}/public/data/#{name}")
+			name=params[:contact][:photo].original_filename
+			directory="public/data"
+			path=File.join(directory,name)
+			File.open(path, "wb") { |f| f.write(params[:contact][:photo].read) }
+			return name
 		end
 	end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140619092631) do
+ActiveRecord::Schema.define(version: 20140620064105) do
 
   create_table "contacts", force: true do |t|
     t.string   "firstname"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140619092631) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image_location"
+    t.string   "city"
   end
 
 end
